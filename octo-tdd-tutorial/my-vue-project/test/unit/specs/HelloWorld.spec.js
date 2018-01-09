543c8dc2b947e5a9a38980401eb7abf13f8ae651
Isidro López <islomar@gmail.com>
2018-01-09 23:45:41 +0100

    Add test directly providing the variable to the test constructor

--- octo-tdd-tutorial/my-vue-project/test/unit/specs/HelloWorld.spec.js
+++ octo-tdd-tutorial/my-vue-project/test/unit/specs/HelloWorld.spec.js
@@ -2,10 +2,27 @@ import Vue from 'vue'
 import HelloWorld from '@/components/HelloWorld'
 
 describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
+  let vm
+
+  beforeEach(function () {
     const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
+    vm = new Constructor().$mount()
+  })
+
+  it('should check that msg is Welcome to Your Vue.js App', () => {
+    expect(vm.$data.msg).to.equal('Welcome to Your Vue.js App')
+  })
+
+  // Another solution is to directly provide the variable to the test constructor.
+  it('should render correct contents', () => {
+    const data = {
+      data: {
+        msg: 'plop'
+      }
+    }
+    const Constructor = Vue.extend(HelloWorld)
+    vm = new Constructor(data).$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
-    .to.equal('Welcome to Your Vue.js App')
+      .to.equal('plop')
   })
 })
